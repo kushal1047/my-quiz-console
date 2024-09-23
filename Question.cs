03795896958d98f77a043aacca512d1a1c447a51
Kushal Niraula <kushal463334@gmail.com>
2024-09-23 10:37:00 +1000

    Build a new quiz app with a Question class

---
+++ Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizConsole
+{
+    internal class Question
+    {
+        public string QuestionText { get; set; }
+        public string[] Answers { get; set; }
+        public int CorrectAnswerIndex { get; set; }
+
+        public Question(string questionText, string[] answers, int correctAnswerIndex)
+        {
+            QuestionText = questionText;
+            Answers = answers;
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
+
+        public bool IsCorrectAnswer(int choice) 
+        { 
+            return choice == CorrectAnswerIndex;
+        }
+    }
+}
