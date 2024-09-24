183ca3403f3854da65bab5130a0b4b133ee3ce45
Kushal Niraula <kushal463334@gmail.com>
2024-09-25 01:11:00 +1000

    Create Quiz class and use this keyword to initialize questions array

---
+++ Quiz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizConsole
+{
+    internal class Quiz
+    {
+        private Question[] questions;
+
+        public Quiz(Question[] questions)
+        {
+            this.questions = questions;
+        }
+    }
+}
--- obj/Debug/net8.0/MyQuizConsole.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyQuizConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyQuizConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03795896958d98f77a043aacca512d1a1c447a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyQuizConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyQuizConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
